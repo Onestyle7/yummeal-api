2e50755b678df53bc274d781f889273e58ab808d
Onestyle7 <klusbartek8@gmail.com>
2025-06-10 18:43:40 +0200

    Add Supabase configuration and AuthController implementation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var supabaseUrl = builder.Configuration["Supabase:Url"];
+var supaBaseKey = builder.Configuration["Supabase:Key"];
+builder.Services.AddSingleton(provider => new Supabase.Client(supabaseUrl, supaBaseKey, new Supabase.SupabaseOptions
+{
+    AutoConnectRealtime = true,
+}));
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
